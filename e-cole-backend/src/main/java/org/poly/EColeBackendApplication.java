2ce0dd6a9fcd0b38c81d53bfa553a7cad7eb3b04
Safouane Jelassi <safouane.jelassi@polytechnicien.tn>
2020-11-09 14:00:19 +0100

    commit creation

---
+++ e-cole-backend/src/main/java/org/poly/EColeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EColeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EColeBackendApplication.class, args);
+	}
+
+}
---
+++ e-cole-backend/src/test/java/org/poly/EColeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.poly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EColeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
