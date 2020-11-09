e0368f85b4e5ea6011c438108063bb80ffa57746
Safouane Jelassi <safouane.jelassi@polytechnicien.tn>
2020-11-09 15:57:52 +0100

    firstClasse

--- e-cole-backend/src/main/java/org/poly/entities/Etudiant.java
+++ e-cole-backend/src/main/java/org/poly/entities/Etudiant.java
@@ -1,8 +1,30 @@
 package org.poly.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Entity
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @AllArgsConstructor @NoArgsConstructor
 public class Etudiant {
-
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String matricule;
+	private String nom ;
+	private String prenom ;
+	private String dateDeNaissance ;
+	private String lieuDeNaissance ;
+	private String adresseActuelle ;
+	private String AdresseOrigine ;
+	private String email ;
+	private String sexe ;
+	private Long cin; 
+	private String photo ;
+	private String numTel ; 
+	
 }
